cf7e7387664f36bb7e219fb5ca67f23982b33508
Stevepar <chetanparalikar86@gmail.com>
2020-08-01 11:17:27 +0530

    modified

--- edx-Django/Project2/commerce/auctions/admin.py
+++ edx-Django/Project2/commerce/auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(listings)
+admin.site.register(bid)
+admin.site.register(comments)
--- edx-Django/Project2/commerce/auctions/models.py
+++ edx-Django/Project2/commerce/auctions/models.py
@@ -1,6 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+Categories = [
+    ('FS', 'Fashion'),
+    ('EC', 'Electronics'),
+    ('BE', 'Beauty'),
+    ('HF', 'Home & Furniture'),
+    ('SP', 'Sports'),
+    ('BK', 'Books'),
+    ('CR', 'Cars')
+]
 
 class User(AbstractUser):
+    pass
+
+class listings(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    item_image = models.URLField(blank=True)
+    item_title = models.CharField(max_length=128)
+    item_des = models.TextField()
+    item_price = models.FloatField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    item_category = models.CharField(choices = Categories, max_length=2)
+    avail_item = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.item_id}, {self.item_title}"
+
+class bid(models.Model):
+    pass
+
+class comments(models.Model):
     pass
